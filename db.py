152056cc4add4e6cd4ac4c1a59e501b9f1d54f72
Oleh <zaaqq135@gmail.com>
2025-08-18 21:50:30 +0300

    created models and updated schemas

--- db.py
+++ db.py
@@ -1,7 +1,3 @@
-stores = {
-    "861bfdcf14c1454391d336dc2ffcdd1f": {
-        "name": "Shop",
-        "id": "861bfdcf14c1454391d336dc2ffcdd1f",
-    }
-}
-items = {}
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()

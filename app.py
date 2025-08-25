faeb775e17c6cf3647ea0c8397fe61ffc3a569f7
Oleh <zaaqq135@gmail.com>
2025-08-25 22:41:53 +0300

    added new env variable

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = (
         "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     )
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DB_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "1234")
